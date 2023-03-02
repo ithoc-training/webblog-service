e4a90eb90ba1b89c4d5b7a9a7eb98ff927f6ded4
Hock <oliver.hock@mecom.de>
2023-03-02 11:19:13 +0100

    deploy docker container via script

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,20 @@
+
+# Shall be done through .bashrc
+eval $(minikube docker-env)
+minikube start
+
+# Optional removal of old stuff
 kubectl delete -n default deployment webblog-service
 docker rmi --force webblog-service:0.0.1-SNAPSHOT
-mvn clean package
-docker build --no-cache --tag webblog-service:0.0.1-SNAPSHOT .
-docker images | grep webblog-service
+
+# Maybe package goal is sufficient
+mvn clean install
+
+#docker build --build-arg DOCKER_PROXY='docker-proxy.mecom.de/' --tag webblog-service:0.0.1-SNAPSHOT .
+#docker images | grep webblog-service
+
+# Build Docker image from and load it directly to Minikube
+minikube image build -t webblog-service:0.0.1-SNAPSHOT .
+
 kubectl apply -f kubernetes-deployment.yaml
 minikube service --all &
